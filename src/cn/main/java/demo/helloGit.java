c56338e278287a315af499ac9268ad8ff6baa3a5
YiYi <YiYi@163.com>
2023-03-29 19:30:13 +0800

    second commit

--- src/cn/main/java/demo/helloGit.java
+++ src/cn/main/java/demo/helloGit.java
@@ -3,5 +3,8 @@ package cn.main.java.demo;
 public class helloGit {
     public static void main(String[] args) {
         System.out.println("Hello git1");
+        System.out.println("Hello git2");
+        System.out.println("Hello git3");
+
     }
 }
